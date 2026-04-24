a2e9edcbf405f1f38117b2777bab184e12093707
IlhanKazan <ilhan.kazan23@gmail.com>
2026-04-25 01:55:48 +0300

    feat(common-lib): EventConstants'a AGGREGATE_TENANT ve tenant event sabitleri eklendi

--- backend/common-lib/src/main/java/com/ecommerce/common/event/constants/EventConstants.java
+++ backend/common-lib/src/main/java/com/ecommerce/common/event/constants/EventConstants.java
@@ -10,6 +10,7 @@ public final class EventConstants {
     public static final String AGGREGATE_ORDER = "ORDER";
     public static final String AGGREGATE_STOCK = "STOCK";
     public static final String AGGREGATE_PAYMENT = "PAYMENT";
+    public static final String AGGREGATE_TENANT = "TENANT";
 
     public static final String EVENT_PRODUCT_CREATED = "PRODUCT_CREATED_EVENT";
     public static final String EVENT_PRODUCT_UPDATED = "PRODUCT_UPDATED_EVENT";
@@ -22,5 +23,9 @@ public final class EventConstants {
     public static final String EVENT_PAYMENT_SUCCESS = "PAYMENT_SUCCESS_EVENT";
     public static final String EVENT_PAYMENT_FAILED = "PAYMENT_FAILED_EVENT";
     public static final String EVENT_SUBSCRIPTION_ACTIVATED = "SUBSCRIPTION_ACTIVATED_EVENT";
+
+    public static final String EVENT_TENANT_CREATED = "TENANT_CREATED_EVENT";
+    public static final String EVENT_TENANT_ACTIVATED = "TENANT_ACTIVATED_EVENT";
+    public static final String EVENT_TENANT_PAYMENT_FAILED = "TENANT_PAYMENT_FAILED_EVENT";
 
 }
